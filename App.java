429c702de828245f34794e5a94e6224f39c357ba
anagha-html <72094734+anagha-html@users.noreply.github.com>
2026-06-02 13:26:15 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,12 @@
+package typebloom;
+
+import javax.swing.SwingUtilities;
+
+public class App {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MainWindow window = new MainWindow();
+            window.setVisible(true);
+        });
+    }
+}
